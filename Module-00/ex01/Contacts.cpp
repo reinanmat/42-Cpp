3b5f5b020e1cce2b3db247a7e56be2d72a746a15
reinanmat <reinan266@gmail.com>
2023-08-04 19:47:53 -0300

    refactor: Contacts class

--- Module-00/ex01/Contacts.cpp
+++ Module-00/ex01/Contacts.cpp
@@ -6,32 +6,81 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:09:56 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/28 18:40:13 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 16:06:13 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contacts.hpp"
+#include <iomanip>
 
 Contacts::Contacts() {}
 
 Contacts::~Contacts() {}
 
-void	Contacts::createContact(int index)
+void	Contacts::new_contact(int index)
 {	
-	this->index = index;
-	std::cout << "First Name: ";
-	std::cin >> this->firstName;
-	std::cout << std::endl;
-	std::cout << "Last Name: ";
-	std::cin >> this->lastName;	
-	std::cout << std::endl;
-	std::cout << "Nickname: ";
-	std::cin >> this->nickname;	
-	std::cout << std::endl;
-	std::cout << "Phone Number: ";
-	std::cin >> this->phoneNumber;	
-	std::cout << std::endl;
-	std::cout << "Darkest secret: ";
-	std::cin >> this->darkestSecret;	
-	std::cout << std::endl;
+	this->index = index + 1;
+	this->first_name = get_info("First Name: ");
+	this->last_name = get_info("Last Name: ");
+	this->nickname = get_info("Nickname: ");
+	this->phone_number = get_number();
+	this->darkest_secret = get_info("Darkest secret: ");
 }
+
+void	Contacts::display_contact()
+{
+	std::cout << "|";
+	std::cout << std::setfill(' ') << std::setw(10);
+	std::cout << this->index;
+	/* std::cout << this->first_name; */
+	/* std::cout << this->last_name; */
+	/* std::cout << this->nickname; */
+	std::cout << "|" << std::endl;
+}
+
+bool	isnumber(std::string s)
+{
+	for (int i = 0; s[i]; i++)
+		if (std::isalnum(s[i]) == 0)
+			return (false);
+	return (true);
+}
+
+int			Contacts::get_number()
+{
+	int			phone_number;
+	std::string	input;
+
+	std::cout << "Phone number: ";
+	while (1)
+	{
+		std::getline(std::cin, input);	
+		if (isnumber(input))
+			break ;
+		
+	}
+	phone_number = 1;
+	return (phone_number);
+}
+
+std::string	Contacts::get_info(std::string wanted_info)
+{
+	std::string	 info;
+
+	std::cout << wanted_info;
+	std::getline(std::cin, info);
+
+	return (info);
+}
+
+Contacts& Contacts::operator=(Contacts other)
+{
+	this->index = other.index;
+	this->nickname = other.nickname;
+	this->first_name = other.first_name;
+	this->last_name = other.last_name;
+	this->phone_number = other.phone_number;
+	this->darkest_secret = other.darkest_secret;
+	return *this;	
+}
+
--- Module-00/ex01/Contacts.hpp
+++ Module-00/ex01/Contacts.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:07:28 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/28 18:40:05 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:26:00 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,18 @@ class Contacts
 	public:
 		Contacts();
 		~Contacts();
-		void createContact(int index);
+		void		new_contact(int index);
+		void		display_contact();
+		int			get_number();
+		std::string	get_info(std::string info);
+		Contacts& operator=(Contacts other);
 	private:
 		int			index;
-		int			phoneNumber;
-		std::string	firstName;
-		std::string	lastName;
+		int			phone_number;
+		std::string	first_name;
+		std::string	last_name;
 		std::string nickname;
-		std::string	darkestSecret;
+		std::string	darkest_secret;
 };
 
 #endif
