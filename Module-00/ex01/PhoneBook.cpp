830c2a9fcb387d43b1b31c8324fbce133e92aa03
reinanmat <reinan266@gmail.com>
2023-08-04 19:48:43 -0300

    refactor: PhoneBook class

--- Module-00/ex01/PhoneBook.cpp
+++ Module-00/ex01/PhoneBook.cpp
@@ -6,24 +6,41 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:04:57 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/28 18:47:34 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:59:28 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(void) {}
+PhoneBook::PhoneBook(void)
+{
+	amount_of_contacts = 0;
+}
 
 PhoneBook::~PhoneBook(void) {}
 
-void	PhoneBook::addContact()
+void	PhoneBook::add_contact()
 {
 	static int	index = 0;
-	Contacts	newContact;
+	Contacts	contact;
 
 	if (index == 8)
 		index = 0;
-	this->contacts[index] = newContact.createContact(index);
+	contact.new_contact(index);
+	this->contacts[index] = contact;
+
+	if (amount_of_contacts != 7)
+		amount_of_contacts++;
+	index++;
 }
 
-/* void	PhoneBook::searchContact(int index) {} */
+void	PhoneBook::display_all_contacts()
+{
+	for (int i = 0; i != amount_of_contacts; i++)
+		contacts[i].display_contact();
+}
+
+void	PhoneBook::search_contact()
+{
+	display_all_contacts();
+}
--- Module-00/ex01/PhoneBook.hpp
+++ Module-00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:39:26 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/28 18:46:35 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:59:12 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 class PhoneBook
 {
 	public:
-		PhoneBook(void);
-		void	addContact();
-		void	searchContact(int index);
-		~PhoneBook(void);
+		PhoneBook();
+		void	add_contact();
+		void	display_all_contacts();
+		void	search_contact();
+		~PhoneBook();
 	private:
-		Contacts contacts[8];
+		int			amount_of_contacts;
+		Contacts	contacts[8];
 };
 
 #endif
