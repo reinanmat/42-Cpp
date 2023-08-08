5e04f0bfd8b029a92d8279b2ac8e130f6636d559
reinanmat <reinan266@gmail.com>
2023-08-08 16:28:29 -0300

    refactor: reworked in main.cpp functions

--- Module-00/ex01/main.cpp
+++ Module-00/ex01/main.cpp
@@ -6,32 +6,57 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:46:04 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/04 16:07:16 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/08 15:40:01 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
+void	addNewContact(PhoneBook& phone)
+{
+	static int	index = 0;
+	std::string	info[6];
+
+	
+	phone.addContact(index, info);
+	if (index == 8)
+		index = 0;
+}
+
+void	seachContact(PhoneBook& phone)
+{
+	std::string	index;
+
+	phone.displayAllContacts();	
+	std::cout << "Choose a contact:" << std::endl;
+	std::getline(std::cin, index);
+	/* phone.displaySpecificContact(index); */
+}
+
 void	startPhone(void)
 {
-	std::string	chosen;
 	PhoneBook	phone;
+	std::string	chosen;
 	
-	std::cout << "Welcome to my PhoneBook" << std::endl;
+	std::cout << "Welcome to My Awesome PhoneBook!" << std::endl;
+	std::cout << "Please choose an option:" << std::endl;
 	while (1)
 	{
 		std::cout << "1.ADD" << std::endl;
 		std::cout << "2.SEARCH" << std::endl;
 		std::cout << "3.EXIT" << std::endl;
 		std::getline(std::cin, chosen);
-		if (chosen.compare("ADD") || chosen.compare("1") == 0)
-			phone.add_contact();
+		if (chosen.compare("ADD") == 0 || chosen.compare("1") == 0)
+			addNewContact(phone);
 		else if (chosen.compare("2") == 0)
-			phone.search_contact();
+			seachContact(phone);
 		else if (chosen.compare("3") == 0)
+		{
+			std::cout << "Bye :)" << std::endl;
 			break ;
+		}
 		else
-			std::cout << "Invalid option please select one of the options below" << std::endl;
+			std::cout << "Invalid option please select one of the options below:" << std::endl;
 	}
 }
 
