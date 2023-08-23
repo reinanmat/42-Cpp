7331aa2e4e1799b34326faaca24d0e39765c4c08
reinanmat <reinan266@gmail.com>
2023-08-23 18:12:07 -0300

    feat: added base ex02

---
+++ Module-00/ex02/Account.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 20:01:50 by revieira          #+#    #+#             */
+/*   Updated: 2023/08/04 20:10:30 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ACCOUNT_H
+# define ACCOUNT_H
+
+#include <iostream>
+
+class Account
+{
+	public:
+		Account();
+		void	displayAccountsInfos();
+		~Account();
+	std::vector<t>;
+	std::vector<int>;
+	std::pair<accounts_t::iterator, ints_t::iterator>;
+};
+
+#endif
