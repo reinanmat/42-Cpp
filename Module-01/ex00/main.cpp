fa180b4b16d32493e337dc119cc5637a6a29e820
reinanmat <reinan266@gmail.com>
2023-08-28 18:26:19 -0300

    feat: added a simple test for class zombie

--- Module-01/ex00/main.cpp
+++ Module-01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 17:17:23 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/23 18:09:04 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/28 18:22:31 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	main(void)
 {
+	Zombie *zombie1;
 
+	zombie1 = newZombie("zombie1");
+	zombie1->announce();
+	delete zombie1;
+	return (0);
 }
