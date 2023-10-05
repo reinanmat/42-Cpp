2a91bafe55efa6d55a41a3783e792a0289cb46d9
reinanmat <reinan266@gmail.com>
2023-10-05 13:47:10 -0300

    feat: ex02

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:34:15 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/02 19:42:00 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/05 13:40:50 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@
 int	main(void)
 {
 	std::string	string = "HI THIS IS BRAIN";
-	std::string	stringPTR;
-	std::string	stringREF;
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = *&string;
 
-	stringPTR = string;
-	stringREF = string;
+	std::cout << "The memory address of the STRING: " << &string << std::endl;
+	std::cout << "The memory address of the STRINGPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address of the STRINGREF: " << &stringREF << std::endl;
+
+	std::cout << "The memory address of the STRING: " << string << std::endl;
+	std::cout << "The memory address of the STRINGPTR: " << *stringPTR << std::endl;
+	std::cout << "The memory address of the STRINGREF: " << stringREF << std::endl;
+	return (0);
 }
