dbd677f135140772e3e0a3508f905d691e383faa
reinanmat <reinan266@gmail.com>
2023-10-13 13:55:41 -0300

    refactor: messages

--- Module-01/ex05/Harl.cpp
+++ Module-01/ex05/Harl.cpp
@@ -6,26 +6,30 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 18:53:05 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/04 19:59:53 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:50:30 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+/* CONSTRUCTORS AND DESTRUCTOR */
 Harl::Harl() {}
 
 Harl::~Harl() {}
 
+/* MEMBER FUNCTIONS */
 void	Harl::debug(void)
 {
-	std::cout << "[DEBUG]: I love having extra bacon for my" <<
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my" <<
 		" 7XL-double-cheese-triple-pickle-specialketchup" <<
 		" burger. I really do!" << std::endl;
 }
 
 void	Harl::info()
 {
-	std::cout << "[INFO]: I cannot believe adding extra" <<
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra" <<
 		" bacon costs more money. You didn’t put" << 
  		" enough bacon in my burger! If you did," <<
 		" I wouldn’t be asking for more!" << std::endl;
@@ -33,13 +37,16 @@ void	Harl::info()
 
 void	Harl::warning(void)
 {
-	std::cout << "[WARNING] This is unacceptable! I want to" <<
-		" speak to the manager now." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you" <<
+		" started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "[ERROR]: This is unacceptable!" <<
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable!" <<
 		" I want to speak to the manager now." << std::endl;
 }
 
