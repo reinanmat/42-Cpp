50e8c1de3d3e82dcad586be2b3c70552283ad915
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-20 18:58:15 -0300

    fix: msgs

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:34:15 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/05 13:40:50 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:59:36 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int	main(void)
 	std::string	*stringPTR = &string;
 	std::string	&stringREF = *&string;
 
-	std::cout << "The memory address of the STRING: " << &string << std::endl;
-	std::cout << "The memory address of the STRINGPTR: " << stringPTR << std::endl;
-	std::cout << "The memory address of the STRINGREF: " << &stringREF << std::endl;
+	std::cout << "The memory address of the STRING:\t" << &string << std::endl;
+	std::cout << "The memory address held by STRINGPTR:\t" << stringPTR << std::endl;
+	std::cout << "The memory address held by STRINGREF:\t" << &stringREF << std::endl;
 
-	std::cout << "The memory address of the STRING: " << string << std::endl;
-	std::cout << "The memory address of the STRINGPTR: " << *stringPTR << std::endl;
-	std::cout << "The memory address of the STRINGREF: " << stringREF << std::endl;
+	std::cout << "The value of the STRING:\t\t" << string << std::endl;
+	std::cout << "The value pointed to by STRINGPTR:\t" << *stringPTR << std::endl;
+	std::cout << "The value pointed to by STRINGREF:\t" << stringREF << std::endl;
 	return (0);
 }
