1f0263797c60d618c09e03ac9be10f772120fc19
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-20 14:45:43 -0300

    fix: indentation

--- Module-00/ex00/megaphone.cpp
+++ Module-00/ex00/megaphone.cpp
@@ -13,7 +13,7 @@ int	main(int argc, char **argv)
 
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	} else { 
+	} else {
 		argv++;
 		for (int i = 0; i < argc - 1; i++) {
 			str = argv[i];
