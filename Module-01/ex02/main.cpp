e6583caef304a78f74209ddaaafaabd5c18f3853
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-26 16:57:26 -0300

    refactor: tests

--- Module-01/ex02/main.cpp
+++ Module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:34:15 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/20 18:59:36 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/26 16:54:42 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(void)
 {
 	std::string	string = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &string;
-	std::string	&stringREF = *&string;
+	std::string	&stringREF = string;
 
 	std::cout << "The memory address of the STRING:\t" << &string << std::endl;
 	std::cout << "The memory address held by STRINGPTR:\t" << stringPTR << std::endl;
