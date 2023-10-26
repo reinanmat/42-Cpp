c0256c7d6f777c782f41f72a7331d48c12944cd3
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-26 15:12:42 -0300

    refactor: tests

--- Module-01/ex01/main.cpp
+++ Module-01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:14:38 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/23 13:45:04 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:55:17 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,29 @@
 
 int	main(void)
 {
-	Zombie		*horde;
-	const int	hordeSize = 8;
-	
-	horde = zombieHorde(hordeSize, "zombie");
-	for (int i = 0; i < hordeSize; i++) {
-		horde[i].announce();
+	{
+		std::cout << "TEST 1" << std::endl;
+		Zombie		*horde;
+		const int	hordeSize = 10;
+
+		horde = zombieHorde(hordeSize, "Bob");
+		for (int i = 0; i < hordeSize; i++) {
+			horde[i].announce();
+		}
+		delete[] horde;
 	}
-	delete[] horde;
+	{
+		std::cout << "TEST 2" << std::endl;
+		Zombie		*horde;
+		const int	hordeSize = 5;
+
+		horde = zombieHorde(hordeSize, "Ignored Zombie");
+		horde[0].setName("First Zombie");
+		horde[hordeSize/2].setName("Middle Zombie");
+		horde[hordeSize - 1].setName("Last Zombie");
+		for (int i = 0; i < hordeSize; i++) {
+			horde[i].announce();
+		}
+		delete[] horde;
+	}
 }
