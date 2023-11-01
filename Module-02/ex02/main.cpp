b1ca7702d3e130bc7fde5eaf56d39ea37f3d4799
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-11-01 16:16:52 -0300

    feat: added tests for other operators

--- Module-02/ex02/main.cpp
+++ Module-02/ex02/main.cpp
@@ -6,46 +6,60 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 14:55:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/06 16:04:29 by revieira         ###   ########.fr       */
+/*   Updated: 2023/11/01 15:55:46 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-void	test(void)
-{
-	if (Fixed(0) == Fixed(0))
-		std::cout << "equal" << std::endl;
-	if (Fixed(3.14f) == Fixed(3.14f))
-		std::cout << "equal" << std::endl;
-	if (Fixed(3.15f) != Fixed(3.14f))
-		std::cout << "different" << std::endl;
-	if (Fixed(3.15f) > Fixed(3.14f))
-		std::cout << "bigger" << std::endl;
-	if (Fixed(1) < Fixed(3))
-		std::cout << "smaller" << std::endl;
-	if (Fixed(1) < Fixed(3))
-		std::cout << "smaller" << std::endl;
-	if (Fixed(0.5f) <= Fixed(0.5f))
-		std::cout << "smaller or equal" << std::endl;
-	if (Fixed(1.2f) >= Fixed(1.2f))
-		std::cout << "bigger or equal" << std::endl;
-}
-
 int	main(void)
 {
-	Fixed	a;
-	Fixed	b( Fixed(5.05f) * Fixed(2));
-	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	{
+		std::cout << "TEST 1" << std::endl;
+		Fixed	a;
+		Fixed	b( Fixed(5.05f) * Fixed(2));
 
-	std::cout << b << std::endl;
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
 
-	std::cout << Fixed::max(a, b) << std::endl;
+		std::cout << b << std::endl;
 
+		std::cout << Fixed::max(a, b) << std::endl;
+	}
+	{
+		std::cout << "TEST 2" << std::endl;
+		Fixed a(0);
+		Fixed b(0);
+		if (a == b)
+			std::cout << a << " is equal to " << b << std::endl;
+
+		a = Fixed(1);
+		b = Fixed(1.3f);
+		if (a != b)
+			std::cout << a << " is different to " << b << std::endl;
+
+		a = Fixed(256);
+		b = Fixed(1);
+		if (a > b)
+			std::cout << a << " is greater to " << b << std::endl;
+
+		a = Fixed(1);
+		b = Fixed(256);
+		if (a < b)
+			std::cout << a << " is less than " << b << std::endl;
+
+		a = Fixed(42.25f);
+		b = Fixed(0);
+		if (a >= b)
+			std::cout << a << " is greater than or equal to " << b << std::endl;
+
+		a = Fixed(0);
+		b = Fixed(42.25f);
+		if (a <= b)
+			std::cout << a << " is less than or equal to " << b << std::endl;
+	}
 	return (0);
 }
