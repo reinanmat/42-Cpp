777590ad08c368cdd565d69f76f20ba1b8008125
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-31 21:32:06 -0300

    refactor: reworked ex03

--- Module-02/ex03/bsp.cpp
+++ Module-02/ex03/bsp.cpp
@@ -6,38 +6,34 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 14:27:21 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/13 16:57:04 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/31 20:44:03 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-float	dotProduct(Point a, Point b)
+Fixed	calculateAreaOfTriangle(Point const a, Point const b, const Point c)
 {
-	return (a.getX() * b.getX() + a.getY() * b.getY());
+	Fixed	area;
+	
+	area = Fixed(0.5f) * Fixed(std::fabs(a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())));
+	return (area);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Point	v1, v2, v3;
-	
-	v1 = c - a;
-	v2 = b - a;
-	v3 = point - a;
+	Fixed	triangle;
+	Fixed	triangleABP;
+	Fixed	triangleBCP;
+	Fixed	triangleACP;
 
-	float dot00 = dotProduct(v2, v1);
-	float dot01 = dotProduct(v1, v2);
-	float dot02 = dotProduct(v1, v3);
-	float dot11 = dotProduct(v2, v2);
-	float dot12 = dotProduct(v2, v3);
-
-	float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-	float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-	std::cout << invDenom << std::endl;
-	std::cout << "u: " << u << std::endl;
-	std::cout << "v: " << v << std::endl;
-	return ((u >= 0) && (v >= 0) && (u + v < 1));
+	triangle = calculateAreaOfTriangle(a, b, c);
+	triangleABP = calculateAreaOfTriangle(a, b, point);
+	triangleBCP = calculateAreaOfTriangle(b, c, point);
+	triangleACP = calculateAreaOfTriangle(a, c, point);
+	if (triangle == triangleACP + triangleBCP + triangleABP)
+		return (true);
+	else
+		return (false);
 }
--- Module-02/ex03/main.cpp
+++ Module-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 14:55:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/13 16:57:23 by revieira         ###   ########.fr       */
+/*   Updated: 2023/10/31 20:30:15 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	main(void)
 {
-	Point	v1(0, 0);
-	Point	v2(0, 100);
-	Point	v3(100, 0);
-	Point	point(0.3, 0.3);
+	Point	v1(0, 3);
+	Point	v2(-2, -2);
+	Point	v3(2, -2);
+	Point	point(0, 0);
 
 	std::cout << "The point (" << point.getX() << ", " << point.getY() << ")" << std::endl;
-	if (bsp(point, v1, v2, v3))
+	if (bsp(v1, v2, v3, point))
 		 std::cout << "is inside the triangle"  << std::endl;
 	else
 		std::cout << "is not inside the triangle" << std::endl;
