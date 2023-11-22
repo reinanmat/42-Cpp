82f1cc518e288089273f67c26a2aab4c2b25b940
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-11-22 14:48:11 -0300

    fix: bsp returns false if the point is on the vertex or edge of the triangle

--- Module-02/ex03/bsp.cpp
+++ Module-02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 14:27:21 by revieira          #+#    #+#             */
-/*   Updated: 2023/11/14 15:46:30 by revieira         ###   ########.fr       */
+/*   Updated: 2023/11/22 14:43:38 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ Fixed	calculateAreaOfTriangle(Point const a, Point const b, const Point c)
 	Fixed	area;
 	Fixed	aux;
 	
-	aux = std::fabs(a.getX() * (b.getY() 
-				 - c.getY()) + b.getX() * (c.getY() 
-				 - a.getY()) + c.getX() * (a.getY() - b.getY()));
+	aux = std::fabs(a.getX() * (b.getY() - c.getY()) +
+				 b.getX() * (c.getY() - a.getY()) +
+				 c.getX() * (a.getY()- b.getY()));
 	area = Fixed(0.5f) * aux;
 	return (area);
 }
@@ -36,5 +36,7 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	triangleABP = calculateAreaOfTriangle(a, b, point);
 	triangleBCP = calculateAreaOfTriangle(b, c, point);
 	triangleACP = calculateAreaOfTriangle(a, c, point);
+	if (triangleABP == Fixed(0) || triangleBCP == Fixed(0) || triangleACP == Fixed(0))
+		return (false);
 	return (triangleABC == triangleACP + triangleBCP + triangleABP);
 }
