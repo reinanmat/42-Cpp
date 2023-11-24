35f8986dcfcaed9ebd5652b5a79e69bcd5921c54
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-11-24 14:54:47 -0300

    refactor: update tests ex01

--- Module-03/ex01/main.cpp
+++ Module-03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 17:08:09 by revieira          #+#    #+#             */
-/*   Updated: 2023/11/22 18:18:09 by revieira         ###   ########.fr       */
+/*   Updated: 2023/11/24 14:54:20 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,73 @@ int	main(void)
 {
 	{
 		std::cout << "TEST 1" << std::endl;
+		std::cout << "Constructors:" << std::endl;
+
 		ClapTrap	oal("Oal");
 		ScavTrap	riny("Riny");
+
+		std::cout << std::endl;
 
 		riny.attack(oal.getName());
 		oal.takeDamage(riny.getAttackDamage());
 		oal.printStatus();
 		riny.printStatus();
 		riny.guardGate();
+
+		std::cout << std::endl;
+
+		std::cout << "Destructors:" << std::endl;
 	}
 	std::cout << std::endl;
 	{
 		std::cout << "TEST 2" << std::endl;
-		ClapTrap	oal;
-		ClapTrap	riny;
+		std::cout << "Constructors:" << std::endl;
+
+		ScavTrap	oal;
+		ScavTrap	riny;
+
+		std::cout << std::endl;
 
 		oal.printStatus();
 		riny.printStatus();
 
-		riny = ClapTrap("Riny");
+		std::cout << std::endl;
+
+		riny = ScavTrap("Riny");
 		oal = riny;
 		oal.setName("Oal");
 
+		std::cout << std::endl;
+
 		oal.printStatus();
 		riny.printStatus();
+
+		std::cout << std::endl;
+		std::cout << "Destructors:" << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "TEST 3" << std::endl;
+		std::cout << "Constructors:" << std::endl;
+
+		ScavTrap oal("Oal");
+		ClapTrap riny("Riny");
+
+		std::cout << std::endl;
+
+		riny.setAttackDamage(999);
+		oal.printStatus();
+		riny.printStatus();
+
+		riny.attack(oal.getName());
+		oal.takeDamage(riny.getAttackDamage());
+		oal.beRepaired(999);
+
+		oal.printStatus();
+		riny.printStatus();
+
+		std::cout << std::endl;
+		std::cout << "Destructors:" << std::endl;
 	}
 	return (0);
 }
