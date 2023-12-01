643edce842688fb84c70958170488e967abb0711
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-01 17:38:52 -0300

    refactor: reworked tests

--- Module-05/ex00/main.cpp
+++ Module-05/ex00/main.cpp
@@ -6,35 +6,40 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:53:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/25 19:25:15 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:36:16 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+static void	TryCreateABureaucrat(std::string name, int grade)
+{
+	std::cout << "Trying to create the Bureaucrat: " << name;
+	std::cout << ", with grade: " << grade << std::endl;
+	try {
+		Bureaucrat a(name, grade);
+		std::cout << GRN "Bureaucrat created" RESET << std::endl;
+		std::cout << a << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << RED "It was not possible to create the Bureaucrat " << name 
+			<< " because " << e.what() << RESET << std::endl;
+	}
+	std::cout << std::endl;
+}
+
 int	main(void)
 {
 	{
 		std::cout << "TEST 1" << std::endl;
-		try {
-			Bureaucrat a(0, "BureaucratA");
-		} catch (std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
-		try {
-			Bureaucrat b(151, "BureaucratB");
-		} catch (std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
-		try {
-			Bureaucrat c(75, "BureaucratC");
-		} catch (std::exception &e) {
-			std::cerr << e.what() << std::endl;
-		}
+		TryCreateABureaucrat("Bob", 151);
+		TryCreateABureaucrat("Anne", 0);
+		TryCreateABureaucrat("Viktor", 150);
+		TryCreateABureaucrat("Marvin", 1);
 	}
+	std::cout << std::endl;
 	{
-		std::cout << "TEST 2" << std::endl;
-		Bureaucrat a(75, "BureaucratA");
+		std::cout << "TEST 2 (INCREMENT GRADE)" << std::endl;
+		Bureaucrat a("BureaucratA", 75);
 		std::cout << a << std::endl;
 		try {
 			for (int i = 0; i < 10; i++) {
@@ -44,8 +49,11 @@ int	main(void)
 		} catch (std::exception &e) {
 			std::cerr << e.what() << std::endl;
 		}
-		Bureaucrat b(75, "BureaucratB");
-		std::cout << a << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "TEST 3 (DECREMENT GRADE)" << std::endl;
+		Bureaucrat b("BureaucratB", 75);
 		try {
 			for (int i = 0; i < 10; i++) {
 				b.decrementGrade();
