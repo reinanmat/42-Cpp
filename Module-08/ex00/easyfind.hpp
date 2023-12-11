b435d8fc102fd1f16b02a1f9eb65aa9102e759a7
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-11 18:43:55 -0300

    feat: finish template function

--- Module-08/ex00/easyfind.hpp
+++ Module-08/ex00/easyfind.hpp
@@ -6,11 +6,24 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 18:27:23 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/08 18:30:03 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/11 18:34:06 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template <typename T>
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define RESET "\x1B[0m"
 
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+void	easyfind(T array, int find)
+{
+	if (std::find(array.begin(), array.end(), find) == array.end())
+		std::cout << RED "Not Found " << find << RESET << std::endl;
+	else
+		std::cout << GRN "Found " << find << RESET << std::endl;
+}
