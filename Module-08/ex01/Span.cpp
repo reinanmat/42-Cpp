59a9e5b4fa38a0943262edd38a6f544f49f1fc4e
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-12 17:07:03 -0300

    feat: added span class

---
+++ Module-08/ex01/Span.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 14:34:42 by revieira          #+#    #+#             */
+/*   Updated: 2023/12/12 16:19:15 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+/* CONSTRUCTORS AND DESTRUCTORS */
+Span::Span() : _size(0)
+{
+	
+}
+
+Span::Span(size_t size) : _size(size)
+{
+}
+
+Span::Span(const Span &obj)
+{
+	if (this != &obj)
+		*this = obj;
+}
+
+Span::~Span()
+{
+	
+}
+
+/* OPERATORS OVERLOADING */
+Span	&Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		this->_size = other._size;
+		this->_vec = other._vec;
+	}
+	return (*this);
+}
+
+std::ostream &operator<<(std::ostream &out, const Span &obj)
+{
+	std::vector<int> tmp;
+
+	tmp = obj.getVector();
+	out << "size: " << obj.getSize() << std::endl;
+	out << "[";
+	for (std::vector<int>::iterator it = tmp.begin(); it != tmp.end(); it++)
+		out << *it << ((it + 1) != tmp.end() ? ", " : "]");
+	return (out);
+}
+
+/* GETTERS */
+std::vector<int>	Span::getVector(void) const
+{
+	return (this->_vec);
+}
+
+size_t	Span::getSize(void) const
+{
+	return (this->_size);
+}
+
+/* MEMBER FUNCTIONS */
+void	Span::addNumber(int number)
+{
+	if (this->_vec.size() >= this->_size)
+		throw std::out_of_range("Unable to add (int number) maximum capacity reached");
+	else
+		this->_vec.push_back(number);
+}
+
+void	Span::addNumber(std::vector<int>::const_iterator itBegin, std::vector<int>::const_iterator itEnd)
+{
+	while (itBegin != itEnd)
+	{
+		this->addNumber(*itBegin);
+		itBegin++;
+	}
+}
+
+size_t	Span::shortestSpan(void) const
+{
+	std::vector<int>			tmp;
+	std::vector<int>::iterator	it;
+	std::vector<int>::iterator	itNext;
+	size_t	smallest;
+
+	tmp = this->_vec;
+	std::sort(tmp.begin(), tmp.end());
+	it = tmp.begin();
+	for (itNext = (tmp.begin() + 1); it != tmp.end(); itNext++)
+	{
+		if (smallest == 1)
+			break ;
+		if (size_t(*itNext - *it) < smallest)
+			smallest = *itNext - *it;
+		it++;
+	}
+	return (smallest);
+}
+
+size_t	Span::longestSpan(void) const
+{
+	std::vector<int> tmp = this->_vec;
+
+	std::sort(tmp.begin(), tmp.end());
+	return (*(tmp.end() - 1) - *tmp.begin());
+}
--- Module-08/ex01/Span.hpp
+++ Module-08/ex01/Span.hpp
@@ -6,11 +6,18 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 18:45:47 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/11 18:52:43 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/12 15:50:45 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <ostream>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <exception>
+#include <algorithm>
 
 class Span
 {
@@ -18,15 +25,20 @@ class Span
 		Span();
 
 	public:
-		Span(unsigned int size);
+		Span(size_t size);
 		Span(const Span &obj);
 		~Span();
 		Span	&operator=(const Span &other);
-
 		void	addNumber(int number);
-		int		shortestSpan(void);
-		int		longestSpan(void);
+		void	addNumber(std::vector<int>::const_iterator itBegin, std::vector<int>::const_iterator itEnd);
+		size_t				getSize(void) const;
+		std::vector<int>	getVector(void) const;
+		size_t				shortestSpan(void) const;
+		size_t				longestSpan(void) const;
 
 	private:
-		unsigned int	_size;
+		size_t				_size;
+		std::vector<int>	_vec;
 };
+
+std::ostream &operator<<(std::ostream &out, const Span &obj);
