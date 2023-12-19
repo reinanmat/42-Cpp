8991e08b9ac9bf105305957cb9bca59244e91dd4
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-19 17:11:52 -0300

    fix: indentation

--- Module-04/ex00/Cat.hpp
+++ Module-04/ex00/Cat.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:55:17 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/16 15:13:05 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/19 16:24:43 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ class Cat : public Animal
 		Cat();
 		Cat(const Cat &obj);
 		~Cat();
-		Cat	&operator=(Cat const &other);
-		void  makeSound(void) const;
+		Cat		&operator=(Cat const &other);
+		void	makeSound(void) const;
 };
--- Module-04/ex00/Dog.hpp
+++ Module-04/ex00/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 18:13:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/10/16 15:12:05 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/19 16:24:34 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ class Dog : public Animal
 		Dog();
 		Dog(const Dog &obj);
 		~Dog();
-		Dog	&operator=(Dog const &other);
-		void  makeSound(void) const;
+		Dog		&operator=(Dog const &other);
+		void	makeSound(void) const;
 };
