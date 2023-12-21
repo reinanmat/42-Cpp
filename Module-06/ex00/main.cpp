4cc9530cc210565d15aa076f27a8204f9346a3d9
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-21 14:36:29 -0300

    feat: added erro message

--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:48:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/06 15:04:16 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/21 14:35:37 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-		ScalarConverter::convert(argv[1]);
+	if (argc != 2)
+	{
+		std::cerr << "Invalid arguments, please use:" <<std::endl;
+		std::cerr << "./converter [VALUE]" << std::endl;
+		return (1);
+	}
+	ScalarConverter::convert(argv[1]);
 	return (0);
 }
