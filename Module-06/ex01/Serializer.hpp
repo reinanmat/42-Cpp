94e1b313d5877fcc4c0c201ca234a4dbe959a4cb
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-12-21 15:26:27 -0300

    refactor: marking Serializer class non-instantiable

--- Module-06/ex01/Serializer.hpp
+++ Module-06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:06:22 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/07 15:06:01 by revieira         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:26:00 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@
 class Serializer
 {
 	public:
+		static Data			*deserialize(uintptr_t raw);
+		static uintptr_t	serializer(Data *ptr);
+
+	private:
 		Serializer();
 		Serializer(const Serializer &obj);
 		~Serializer();
 		Serializer	&operator=(const Serializer &other);
-		static Data			*deserialize(uintptr_t raw);
-		static uintptr_t	serializer(Data *ptr);
 };
