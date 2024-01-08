432949cfbea69d6a7f169e47c3c2f4cc5512d846
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-01-08 18:00:58 -0300

    refactor: tests

--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 18:02:33 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/08 17:42:46 by revieira         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:21:08 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,35 @@
 
 int	main(void)
 {
-	int		array_int[5] = {1, 2, 3, 4, 5};
-	float	array_float[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
-	std::string		array_string[5] = {"one", "two", "third", "four", "five"};
-	
-	std::cout << "Array int " << std::endl;
-	::iter(array_int, 5, ::print);
-	std::cout << std::endl;
+	{
+		std::cout << "TEST 1 (INT ARRAY)" << std::endl;
+		int		array[5] = {1, 2, 3, 4, 5};
 
-	std::cout << "Array float " << std::endl;
-	::iter(array_float, 5, ::print);
+		std::cout << "Calling the inter function:" << std::endl;
+		::iter(array, 5, ::print);
+	}
 	std::cout << std::endl;
+	{
+		std::cout << "TEST 2 (FLOAT ARRAY)" << std::endl;
+		float	array[5] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
 
-	std::cout << "Array string " << std::endl;
-	::iter(array_string, 5, ::print);
+		std::cout << "Calling the inter function:" << std::endl;
+		::iter(array, 5, ::print);
+	}
 	std::cout << std::endl;
+	{
+		std::cout << "TEST 3 (DOUBLE ARRAY)" << std::endl;
+		double	array[5] = {1.1111, 2.2222, 3.3333, 4.4444, 5.5555};
+
+		std::cout << "Calling the inter function:" << std::endl;
+		::iter(array, 5, ::print);
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "TEST 4 (STRING ARRAY)" << std::endl;
+		std::string	array[5] = {"one", "two", "third", "four", "five"};
+
+		std::cout << "Calling the inter function:" << std::endl;
+		::iter(array, 5, ::print);
+	}
 }
