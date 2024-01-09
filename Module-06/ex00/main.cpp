5dc3b0ce0af5d5be8da5de7baed855fb99ab278c
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-01-09 16:16:57 -0300

    refactor: error message

--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:48:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/12/21 14:35:37 by revieira         ###   ########.fr       */
+/*   Updated: 2024/01/09 16:13:39 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "Invalid arguments, please use:" <<std::endl;
-		std::cerr << "./converter [VALUE]" << std::endl;
+		std::cerr << "Invalid arguments" <<std::endl;
+		std::cerr << "use: ./converter [VALUE]" << std::endl;
 		return (1);
 	}
 	ScalarConverter::convert(argv[1]);
