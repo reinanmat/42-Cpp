d2b56589e80d457fa95bd77c7077daab46db841d
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-01-11 15:03:09 -0300

    refactor: test

--- Module-07/ex00/main.cpp
+++ Module-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:38:17 by revieira          #+#    #+#             */
-/*   Updated: 2024/01/10 17:03:50 by revieira         ###   ########.fr       */
+/*   Updated: 2024/01/11 15:00:41 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ int	main(void)
 
 		::swap(a, b);
 		std::cout << "a = " << a << ", b = " << b << std::endl;
-		std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-		std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+		std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 		std::string c = "chaine1";
 		std::string d = "chaine2";
 
 		::swap(c, d);
 		std::cout << "c = " << c << ", d = " << d << std::endl;
-		std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-		std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+		std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+		std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 	}
 	std::cout << std::endl;
 	{
