278f9b6c9be05c2a29a8034906eda19f44fc31ed
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-01-11 15:15:33 -0300

    refactor: templates functions

--- Module-07/ex01/iter.hpp
+++ Module-07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:58:53 by revieira          #+#    #+#             */
-/*   Updated: 2024/01/10 17:04:49 by revieira         ###   ########.fr       */
+/*   Updated: 2024/01/11 15:13:59 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 #include <iostream>
 
 template <typename T>
-void	iter(const T array[], size_t size, void f(const T))
+void	iter(T *array, size_t size, void f(T&))
 {
-	for (size_t i = 0; i < size; i++)
-		f(array[i]);
+	if (array)
+		for (size_t i = 0; i < size; i++)
+			f(array[i]);
 }
 
 template <typename T>
-void	print(const T x)
+void	print(T &x)
 {
 	std::cout << x << std::endl;
 }
